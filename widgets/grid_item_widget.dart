f349cb0bd3379117365143f879f081366a182bf5
lampitahts <221113096@students.mikroskil.ac.id>
2024-10-21 02:49:52 +0700

    Update grid_item_widget.dart

--- widgets/grid_item_widget.dart
+++ widgets/grid_item_widget.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+
+class GridItemWidget extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  GridItemWidget({required this.icon, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 40, color: Colors.green[800]),
+              SizedBox(height: 8),
+              Text(label, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
